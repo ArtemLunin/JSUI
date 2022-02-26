60f1922f4c100fb268225900fdd0d9ff7ead1dba
Artem <lunin.mail@gmail.com>
2022-02-26 18:01:04 +0200

    Lesson 39-40

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 import $ from './lib/lib';
 
 $('button').on('click', function() {
-	$(this).toggleClass('active');
+	$('div').eq(2).toggleClass('active');
 });
+
+$('div').click(function() {
+	console.log($(this).index());
+});
+
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'));
+// console.log($('button').html('123'));
+
+console.log($('.findme').siblings());
