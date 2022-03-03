8ed9db64dbaf5caa1bb108308a26e95e74914101
Artem <lunin.mail@gmail.com>
2022-03-03 16:46:17 +0200

    Lesson 46

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,41 @@ $('button').eq(2).on('click', () => {
 	$('.w-500').fadeToggle(800);
 });
 
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'Another btn',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('Hello world');
+                }
+            ]
+        ]
+    }
+}));
+
+
 $('.wrap').html(
 	`
 	<div class="dropdown">
